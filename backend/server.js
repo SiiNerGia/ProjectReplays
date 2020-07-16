4d3e3782660c57bb7a2a6a7f441e1836926fa89d
Victor Rosado <sinergiaftw@gmail.com>
2020-07-16 08:44:59 +0200

    adding message

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 
 require('dotenv').config();
 
@@ -26,6 +27,10 @@ app.use('/replays', replaysRouter);
 app.use('/search', searchRouter);
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('project-replays/build'))
+
+    app.get('*', (req,res) => {
+     res.sendFile(path.join(__dirname, 'client','build','index.html'));    
+    })
 }
 
 app.listen(port, () => {
