3f8087741790af2ee93f229accc9a2703b564a54
Victor Rosado <sinergiaftw@gmail.com>
2020-07-16 09:31:48 +0200

    reestructuracion de el proyecto

---
+++ project-replays/build/precache-manifest.5e8c252a88751c575a4152c54306b87b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19de02d17e0f51bfd2bdb1b259446930",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e5d185d3efe6fdd15d9",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c96030c7fa8e92442553",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7e5d185d3efe6fdd15d9",
+    "url": "/static/js/2.2da4a023.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2da4a023.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c96030c7fa8e92442553",
+    "url": "/static/js/main.32b63c53.chunk.js"
+  },
+  {
+    "revision": "706228c8aa68530772b2",
+    "url": "/static/js/runtime-main.f1e64ddb.js"
+  }
+]);
